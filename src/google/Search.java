910fefe43ecc1f67d57b3ef74cfa45ea62abf4f3
naveenzak <naveenrozakari@gmail.com>
2026-05-26 10:19:25 +0530

    Update Search by Kotesh.java

--- src/google/Search.java
+++ src/google/Search.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.Test;
 
 public class Search {
@@ -11,12 +12,17 @@ public class Search {
   @Test
   public void search() {
 	  driver.get("https://www.google.com");
-	  driver.findElement(By.name("q")).sendKeys("GitHub");
+	  driver.findElement(By.name("q")).sendKeys("Selenium");
   }
   @BeforeTest
   public void beforeTest() {
 	  driver = new ChromeDriver();
 	  driver.manage().window().maximize();
+  }
+
+ @AfterTest
+  public void afterTest() {
+	 driver.quit();
   }
 
 }
