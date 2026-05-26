81717cd60af7b9b1fd1b4c1750eef2e1c14af18a
naveenzak <naveenrozakari@gmail.com>
2026-05-26 10:15:40 +0530

    Search class updated from google package

--- src/google/Search.java
+++ src/google/Search.java
@@ -1,14 +1,22 @@
 package google;
 
-import org.testng.annotations.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 public class Search {
+	WebDriver driver;
   @Test
-  public void f() {
+  public void search() {
+	  driver.get("https://www.google.com");
+	  driver.findElement(By.name("q")).sendKeys("GitHub");
   }
   @BeforeTest
   public void beforeTest() {
+	  driver = new ChromeDriver();
+	  driver.manage().window().maximize();
   }
 
 }
